3c609b73fe20cf39833efa1d3d4e851b62ecfd03
Sanjay Singh Rawat <rawatsanjay2799@gmail.com>
2024-02-21 08:09:51 +0530

    added Establish connection with mysql database

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 const express = require('express');
-
+const mysql = require('mysql2')
 const app = express();
 
+const pool = mysql.createPool(
+    {
+        host: '127.0.0.1',
+        user: '',
+        password: '',
+        database: '',
+        port: 3306,
+        multipleStatements: true
+    });
 
-const port = 3000 
 
-const server = app.listen(port,()=>{
-     console.log('server is running on 3000 port')
+app.get('/', (req, res) => {
+    pool.query('select * from categories', (error, categories) => {
+        if (error) {
+            res.status(400).send(error);
+        } else {
+            res.status(200).send(categories);
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = 3000
+const server = app.listen(port, () => {
+    console.log('server is running on 3000 port')
 })
