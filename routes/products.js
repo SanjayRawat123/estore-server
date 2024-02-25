14898b2000dd13aa83af8786c40411669a4e828c
Sanjay Singh Rawat <rawatsanjay2799@gmail.com>
2024-02-25 20:29:27 +0530

    modified route file project.js

--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,7 @@ const pool = require('../share/pool');
 router.get('/', (req, res) => {
     let mainCategoryId = req.query.mainCategoryId;
     let subCategoryId = req.query.subCategoryId;
-
+    let keyword = req.query.keyword;
     let query = 'select * from products';
     if (subCategoryId) {
         query += ' where category_id =' + subCategoryId;
@@ -15,7 +15,11 @@ router.get('/', (req, res) => {
         query = `select products. * from products , categories 
         where products.category_id = categories.id and categories.parent_category_id
         =${mainCategoryId}`;
-    } 
+    }
+
+    if (keyword) {
+        query += ` and keywords like '%${keyword}%'`;
+    }
     pool.query(query, (error, products) => {
         if (error) {
             res.status(404).send(error);
