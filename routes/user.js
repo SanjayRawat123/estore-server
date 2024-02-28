231621860b7ce8470daa136bcfbd035a33e16034
Sanjay Singh Rawat <rawatsanjay2799@gmail.com>
2024-02-28 07:13:39 +0530

    created login api for user

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pool = require('../share/pool');
 const bcrybtjs = require('bcryptjs');
 const router = express.Router();
-
+const jwtoken = require('jsonwebtoken');
 router.post('/signup', (req, res) => {
 
     try {
@@ -26,7 +26,7 @@ router.post('/signup', (req, res) => {
                     if (resultCount[0].count > 0) {
                         res.status(200).send({ message: 'Email already exists' });
                     } else {
-                        bcryptjs.hash(password, 10).then((hashedPassword) => {
+                        bcrybtjs.hash(password, 10).then((hashedPassword) => {
                             const query = `Insert into users (email,firstName,lastName,address,city,state,pin,password)
                           values
                           ('${email}','${firstName}','${lastName}','${address}','${city}','${state}','${pin}','${hashedPassword}')`;
@@ -53,6 +53,52 @@ router.post('/signup', (req, res) => {
         });
     }
 
+});
+
+
+router.post("/login", (req, res) => {
+    try {
+        let email = req.body.email;
+        let password = req.body.password;
+        pool.query(`select * from users where email like '${email}'`, (error, result) => {
+            if (error) {
+                res.status(500).send({
+                    error: error.code,
+                    message: error.message,
+                });
+            } else {
+                if (result.length > 0) {
+                    bcrybtjs.compare(password, result[0].password).then((compareResult) => {
+                        if (compareResult) {
+                            const token = jwtoken.sign(
+                                { id: result[0].id, email: result[0].email },
+                                'estore-secret-key',
+                                { expiresIn: '1h' }
+                            );
+                            res.status(200).send({
+                                token: token,
+                            });
+                        } else {
+                            res.status(401).send({
+                                message: `Invalid password.`,
+                            });
+                        }
+
+
+                    });
+                } else {
+                    res.status(401).send({
+                        message: `User doesn't exist.`,
+                    });
+                }
+            }
+        })
+    } catch (error) {
+        res.status(400).send({
+            error: error.code,
+            message: error.message,
+        });
+    }
 });
 
 
