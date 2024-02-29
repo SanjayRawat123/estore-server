92cb7a246cb81c3bed915fd184457f3b1067befc
Sanjay Singh Rawat <rawatsanjay2799@gmail.com>
2024-02-29 22:44:53 +0530

    created oder file for created order related API and created post oder APi

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const cors = require('cors');
 const productCategories = require('./routes/productCategroies');
 const products = require('./routes/products');
 const users = require('./routes/user');
+const orders = require('./routes/orders');
 const bodyparser = require('body-parser');
 const app = express();
 
 
 app.use(cors())
 app.use(bodyparser.json());
-app.use('/productCategories', productCategories)
-app.use('/products',products)
-app.use('/users',users)
+app.use('/productCategories', productCategories);
+app.use('/products',products);
+app.use('/users',users);
+app.use('/orders',orders);
 const port = 3000
 const server = app.listen(port, () => {
     console.log('server is running on 3000 port')
