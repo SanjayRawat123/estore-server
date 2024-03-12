c497713263670ad2553fd014957ce2de4181e1ef
Sanjay Singh Rawat <rawatsanjay2799@gmail.com>
2024-03-12 08:11:51 +0530

    changes made in pool.js

--- share/pool.js
+++ share/pool.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool(
         host: process.env.DATABASE_HOST,
         user: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE,
+        database: 'estore',
         port: process.env.DATABASE_PORT,
         multipleStatements: true
     });
